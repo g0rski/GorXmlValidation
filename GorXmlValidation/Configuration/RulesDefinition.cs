dc490042af9266d1c6a9c39fc1e1d507cd9845c1
goral.poczta@gmail.com <goral.poczta@gmail.com>
2015-04-29 18:04:14 +0200

    RuleValidation added Next step in developement process

---
+++ GorXmlValidation/Configuration/RulesDefinition.cs
@@ -0,0 +1,71 @@
+﻿using System.Xml.Serialization;
+
+[XmlType(AnonymousType = true)]
+[XmlRoot(IsNullable = false, ElementName = "applicationValidators")]
+public class RulesDefinition
+{
+    [XmlArray(ElementName = "rules", IsNullable = false)]
+    [XmlArrayItem(ElementName = "rule")]
+    public Rule[] Rules { get; set; }
+}
+
+public class Rule
+{
+    [XmlAttribute(AttributeName = "name")]
+    public string Name { get; set; }
+
+    [XmlArray(ElementName = "conditions", IsNullable = true)]
+    [XmlArrayItem(ElementName = "condition")]
+    public Condition[] Conditions { get; set; }
+
+    [XmlArray(ElementName = "properties", IsNullable = true)]
+    [XmlArrayItem(ElementName = "property")]
+    public ValidationProperty[] Properties { get; set; }
+}
+
+[XmlType(AnonymousType = true)]
+public class Condition
+{
+    [XmlElement(ElementName = "when")]
+    public Case When { get; set; }
+    [XmlElement(ElementName = "then")]
+    public Case Then { get; set; }
+}
+
+[XmlType(AnonymousType = true)]
+public class Case
+{
+    [XmlElement(ElementName = "property")]
+    public ValidationProperty Property { get; set; }
+}
+
+[XmlType(AnonymousType = true)]
+public class ValidationProperty
+{
+    [XmlElement(ElementName = "match")]
+    public SimpeRule Match { get; set; }
+
+    [XmlElement(ElementName = "notEmpty")]
+    public SimpeRule NotEmpty { get; set; }
+
+    [XmlElement(ElementName = "greater")]
+    public SimpeRule Greater { get; set; }
+
+    [XmlElement(ElementName = "lower")]
+    public SimpeRule Lower { get; set; }
+
+    [XmlAttribute(AttributeName = "name")]
+    public string Name { get; set; }
+
+    [XmlAttribute(AttributeName = "type")]
+    public string Type { get; set; }
+}
+
+[XmlType(AnonymousType = true)]
+public class SimpeRule
+{
+    [XmlAttribute(AttributeName = "errorMessageKey")]
+    public string ErrorMessageKey { get; set; }
+    [XmlAttribute(AttributeName = "value")]
+    public string Value { get; set; }
+}
